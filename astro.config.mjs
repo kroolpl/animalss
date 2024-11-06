8528873288d1f310d0d3f88b4e3ca3ba70e594e7
qrol <ggglondyn@gmail.com>
2024-11-06 21:51:19 +0100

    update v0.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,37 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import compress from 'astro-compress';
 
 export default defineConfig({
-  integrations: [tailwind()]
+	site: 'https://animals.glogow.pl',
+	integrations: [
+		tailwind({
+			config: { optimizeCss: true }
+		}),
+		sitemap(),
+		compress({
+			html: true,
+			css: true,
+			js: true,
+			img: false,
+			svg: true
+		})
+	],
+	vite: {
+		build: {
+			cssCodeSplit: true,
+			chunkSizeWarningLimit: 500,
+			rollupOptions: {
+				output: {
+					manualChunks: {
+						vendor: ['/src/scripts/mobileMenu.ts', '/src/scripts/parallax.ts']
+					}
+				}
+			}
+		},
+		ssr: {
+			noExternal: ['@astrojs/tailwind']
+		}
+	}
 });
